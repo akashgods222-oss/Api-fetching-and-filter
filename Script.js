48599fa259d8650ca1e7456dcc53bd5c6a481703
akashgods222-oss <akashgods222@gmail.com>
2026-07-24 21:24:27 +0530

    Add async function to fetch and log women's clothing

--- Script.js
+++ Script.js
@@ -1,1 +1,17 @@
+async function getProducts(){
 
+
+let response = await fetch("https://fakestoreapi.com/products");
+let data = await response.json();
+console.log(data);
+
+let womenClothes = data.filter(item => item.category ==="women's clothing");
+
+for(let item of womenClothes) {
+    
+        console.log(item.title +"| Price: $"+ item.price)
+    
+  }
+}
+
+getProducts();
